10a04b5977325812207b0130fd4a283e98a63b58
shaywacker <shay2442@yahoo.com>
2021-10-05 12:17:31 -0500

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-function NavBar() {
+function NavBar( {home, about}) {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return  <nav>NavBar
+  <a href ='#home'> Home</a>
+  <a href ='#about'>About</a>
+  </nav>
+
+
+
+
 }
 
 export default NavBar;
